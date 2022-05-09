aa997c8c4c97e524c9b26b7da765684aac9536e5
Mego Elhawary <ahmedelhawary@seekasia.com>
2022-05-09 08:14:41 +0800

    - set state

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
+import React, { useState } from 'react';
 import './App.css';
 import GoalList from './components/GoalList/GoalList';
 import NewGoal from './components/NewGoal/NewGoal';
-
 import { Goal } from './types';
 
-const myLifeGoals: Goal[] = [
-  { id: '1', text: 'Eat' },
-  { id: '2', text: 'Hack' },
-  { id: '3', text: 'Sleep' },
-  { id: '4', text: 'Repeat' },
-];
+let goals: Goal[] = [];
+let setGoal: React.Dispatch<React.SetStateAction<Goal[]>>;
 
-export const addNewGoalHandler = (goal: Goal) =>{
-  myLifeGoals.push(goal);
+export const addNewGoalHandler = (newGoal: Goal) => {
+  setGoal((previousGoals)=> previousGoals.concat(newGoal));
 
-  console.log(myLifeGoals);
-}
+  console.log(goals);
+};
 
 const App = () => {
+  [goals, setGoal] = useState([
+    { id: '1', text: 'Eat' },
+    { id: '2', text: 'Hack' },
+    { id: '3', text: 'Sleep' },
+    { id: '4', text: 'Repeat' },
+  ]);
+
   return (
     <div>
       <h2 className="App-header">Life goals</h2>
       <NewGoal onAddGoal={addNewGoalHandler} />
-      <GoalList goals={myLifeGoals} />
+      <GoalList goals={goals} />
     </div>
   );
 };
--- src/components/NewGoal/NewGoal.tsx
+++ src/components/NewGoal/NewGoal.tsx
@@ -3,21 +3,27 @@ import { addNewGoalHandler } from '../../App';
 import { Goal } from '../../types';
 import './NewGoal.css';
 
+let newGoalText = '';
+
 const NewGoal = (props: { onAddGoal: typeof addNewGoalHandler }) => {
   const addGoalHandler = (event: React.FormEvent) => {
     event.preventDefault();
 
     const newGoal: Goal = {
       id: Math.random().toString(),
-      text: 'New Goal !',
+      text: newGoalText,
     };
 
     props.onAddGoal(newGoal);
   };
 
+  const textChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    newGoalText = event.target.value;
+  };
+
   return (
     <form className="new-goal" onSubmit={addGoalHandler}>
-      <input type="text" />
+      <input type="text" onChange={textChangeHandler} />
       <button type="submit">Add Goal</button>
     </form>
   );
